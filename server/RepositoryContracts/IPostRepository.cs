f42edd991126bc18cb9cff7397bde3249ade6642
Mariete_Colequete <mariocuellarprieto@gmail.com>
2024-09-04 21:52:28 +0200

    v0.1.2

--- server/RepositoryContracts/IPostRepository.cs
+++ server/RepositoryContracts/IPostRepository.cs
@@ -6,7 +6,7 @@ public interface IPostRepository
 {
     Task<Post> AddAsync(Post post);
     Task UpdateAsync(Post post);
-    Task DeleteAsync(Post post);
-    Task<Post> GetSingleAsync(Post post);
+    Task DeleteAsync(int id);
+    Task<Post> GetSingleAsync(int id);
     IQueryable<Post> GetMany();
 }
