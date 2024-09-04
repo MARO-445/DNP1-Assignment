f9849fd1f9704430c2a57ce141cd7434c2656401
Mariete_Colequete <mariocuellarprieto@gmail.com>
2024-09-04 20:53:45 +0200

    v0.1.1

---
+++ Entities/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public int UserId { get; set; }
+    public int location { get; set; }
+}
