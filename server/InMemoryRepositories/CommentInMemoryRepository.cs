b52ad0ea8a79232fb2d7cd0c83de27dc3c49cc07
Mariete_Colequete <mariocuellarprieto@gmail.com>
2024-09-04 22:14:37 +0200

    v1.0 - Assignment ready

---
+++ server/InMemoryRepositories/CommentInMemoryRepository.cs
@@ -0,0 +1,63 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepositories;
+
+public class CommentInMemoryRepository : ICommentRepository
+{
+    private List<Comment> comments = new List<Comment>();
+    
+    public Task<Comment> AddAsync(Comment comment)
+    {
+        comment.Id = comments.Any()
+            ? comments.Max(p => p.Id) + 1
+            : 1;
+        comments.Add(comment);
+        return Task.FromResult(comment);
+    }
+
+    public Task UpdateAsync(Comment comment)
+    {
+        Comment? existingComment = comments.SingleOrDefault(p => p.Id == comment.Id);
+        if (existingComment is null)
+        {
+            throw new InvalidOperationException(
+                $"Comment with ID {comment.Id} not found");
+        }
+        
+        comments.Remove(existingComment);
+        comments.Add(comment);
+        
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        Comment? commentToRemove = comments.SingleOrDefault(p => p.Id == id);
+        if (commentToRemove is null)
+        {
+            throw new InvalidOperationException(
+                $"Comment with ID {id} not found");
+        }
+
+        comments.Remove(commentToRemove);
+        return Task.CompletedTask;
+    }
+
+    public Task<Comment> GetSingleAsync(int id)
+    {
+        Comment? commentToReturn = comments.SingleOrDefault(p => p.Id == id);
+        if (commentToReturn is null)
+        {
+            throw new InvalidOperationException(
+                $"Comment with ID {id} not found");
+        }
+        
+        return Task.FromResult(commentToReturn);
+    }
+
+    public IQueryable<Comment> GetMany()
+    {
+        return comments.AsQueryable();
+    }
+}
